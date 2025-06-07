4fa65aa82a4a50cc28097d773289413b40c2ec19
Sergey Samsonov <47633867+Weegley@users.noreply.github.com>
2025-06-07 08:15:58 +0300

    Initial commit
    Сделать:
    - Прятать в трей

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Xml.Schema;
+using static NanoBin.Program;
+
+namespace NanoBin
+{
+
+    public partial class Form1 : Form
+    {
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+
+
+
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace NanoBin
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+
+
+
+
+
+        //static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, RecycleFlags dwFlags);
+
+
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
